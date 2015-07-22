d1c1738a6cfb987e48148448080ebdec28855903
lijippy@163.com <lijippy@163.com>
2015-07-22 17:32:06 +0800

    change log

--- walle/sys/test/test_string_to_sec.cpp
+++ walle/sys/test/test_string_to_sec.cpp
@@ -7,11 +7,22 @@ int main ()
 {
 	const char* date1 = "2015-07-022 17:23:3.2343";
 	const char* date2 = "1990/09/07/10/2/3/2343";
-	string str1 = Time::dateString(date1, "- :.").toDateTime();
+	
+	Time t1  = Time::dateString(date1, "- :.");
+	Time t2 = Time::dateString(date2, "/");
+		
+	cout<<"****date1******"<<date1<<endl;
+	cout<<"string: "<<date1<<endl;
+	cout<<"sec: "<<t1.toSeconds()<<endl;
+	cout<<"usec: "<<t1.toMicroSeconds()<<endl;
+	cout<<"string trans: "<<t1.toDateTime()<<endl;
+	cout<<"*********date2********"<<endl;
+	cout<<"string: "<<date2<<endl;
+	cout<<"sec: "<<t2.toSeconds()<<endl;
+	cout<<"usec: "<<t2.toMicroSeconds()<<endl;
+	cout<<"string trans: "<<t2.toDateTime()<<endl;
 
-	cout<<"date1:"<<str1<<endl;
-	string str2 = Time::dateString(date2, "/").toDateTime();
-	cout<<"date2:"<<str2<<endl;
+	return 0;
 	
 	
 }
